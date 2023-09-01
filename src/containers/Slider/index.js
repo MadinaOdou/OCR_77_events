8bcc907be8308d06911b43569d11e25dd427da3a
Madina <madinaodou@gmail.com>
2023-09-01 11:56:51 +0200

    adding an onChange handler at Slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -48,6 +48,7 @@ const Slider = () => {
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
+                  onChange={(e) => setIndex(e.target.checked)}
                 />
               ))}
             </div>
